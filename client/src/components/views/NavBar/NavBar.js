899b8740f9ed737c393b41446a37e217cc6c460b
Ayushman Tripathi <ayushmantripathi7724@gmail.com>
2023-03-05 02:08:13 +0530

    update navbar logout/login toggle

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -11,6 +12,8 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import Link from '@mui/material/Link';
+import { USER_SERVER } from "../../../Config";
+import { useSelector } from "react-redux";
 import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
 import IntegrationInstructionsIcon from '@mui/icons-material/IntegrationInstructions';
 import './NavBar.css';
@@ -19,7 +22,19 @@ const settings = ['Profile', 'Account', 'Products', 'Logout', 'Follow Me@LinkedI
 function NavBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
-
+  const user = useSelector((state) => state.user);
+  const [visible, setVisible] = useState(false);
+  const logoutHandler = (props) => {
+    axios.get(`${USER_SERVER}/logout`).then((response) => {
+      if (response.status === 200) {
+        window.localStorage.removeItem("userId");
+        window.localStorage.removeItem("remember");
+        
+      } else {
+        alert("Log Out Failed");
+      }
+    });
+  }
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
@@ -34,7 +49,141 @@ function NavBar() {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
-
+  if (user.userData && user.userData.isAuth) {
+    return (
+      <AppBar position="static" enableColorOnDark sx={{ backgroundColor: '#272727' }}>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <IntegrationInstructionsIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              <span id='logo'>MERN_TEMPLATE</span>
+            </Typography>
+  
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                
+                <Link href="#"> <MenuItem key='page' onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">Products</Typography>
+                  </MenuItem></Link>
+  
+                  <Link href="/login">  <MenuItem key='page' onClick={logoutHandler}>
+                    <Typography textAlign="center">LogOut</Typography>
+                  </MenuItem></Link>
+              </Menu>
+            </Box>
+            {/* <IntegrationInstructionsIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href=""
+              sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+             MERN_TEMPLATE
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              
+                <Button
+                  key="Products"
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                Products
+                </Button>
+                <Button
+                  href="/login"
+                  key="Products"
+                  onClick={logoutHandler}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                > LOG OUT
+                </Button>
+              
+            </Box>
+  
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <SettingsApplicationsIcon sx={{color:'white',display: { xs: 'none', md: 'flex' }, mr: 1,fontSize: 40 }} />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    );
+  }
   return (
     <AppBar position="static" enableColorOnDark sx={{ backgroundColor: '#272727' }}>
       <Container maxWidth="xl">
